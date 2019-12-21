2a34898075a1a2979f915b263a52b8a7be9310f1
Matvey <matvey.w2015@mail.ru>
2019-12-21 23:55:40 +0400

    add russian language section

--- js/main.js
+++ js/main.js
@@ -2,4 +2,14 @@ $(function () {
     $('#select').select2({
         placeholder: "Select Services",
     });
+
+    $('.header__localization').click(function () {
+        $('div.dropdown-content').toggle();
+        if (!$('.header__localization-arrow').hasClass('rotate')) {
+            $('.header__localization-arrow').addClass('rotate');
+        } else {
+            $('.header__localization-arrow').removeClass('rotate');
+        }
+
+    });
 });
