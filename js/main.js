c92293e3f310b4524678577d3527696a24f297e5
Matvey <matvey.w2015@mail.ru>
2019-12-30 17:33:54 +0400

    adaptive done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,55 @@
+function backToTop() {
+    let button = $('.svg');
+
+    $(window).on('scroll', () => {
+        if ($(this).scrollTop() >= 150) {
+            button.fadeIn();
+        } else {
+            button.fadeOut();
+        }
+    });
+
+    button.on('click', (e) => {
+        e.preventDefault();
+        $('html').animate({
+            scrollTop: 0
+        }, 1200);
+    })
+}
+backToTop();
+
+$(window).resize(function () {
+    if ($(window).width() <= 1080) {
+        $('.header__menu li').removeClass('hvr-underline-from-left');
+        $('.header__menu li').removeAttr('style');
+    } else {
+        $('.header__menu li').addClass('hvr-underline-from-left');
+        $('.header__menu').removeAttr('style');
+        $('.header__menu li').last().attr('style', 'display:none;');
+    };
+});
+
+
 $(function () {
+    $('.burger').on('click', () => {
+
+        $('.header__menu').slideToggle();
+    });
+
+    $('.header__info-btn').hover(function () {
+        if ($('.header__info-btn').hasClass('animated')) {
+            $('.header__info-btn').removeClass('animated');
+        } else {
+            $('.header__info-btn').addClass('animated');
+        }
+
+    });
+
+    $(".js-example-basic-single").select2({
+        dropdownParent: $('.select2-selection__arrow'),
+    });
+
+
     $('#select').select2({
         placeholder: "Select Services",
     });
@@ -16,4 +67,18 @@ $(function () {
         }
 
     });
+
+    $('.navigation a').on('click', function (e) {
+        if (this.hash !== '') {
+            e.preventDefault();
+
+            const hash = this.hash;
+
+            $('html, body')
+                .animate({
+                    scrollTop: $(hash).offset().top
+                }, 600);
+        }
+    });
+
 });
